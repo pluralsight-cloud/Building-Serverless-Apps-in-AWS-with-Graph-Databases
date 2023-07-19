a19e7a5aaf627206f5af8b29b6ad0e0399eb1e23
Travis Thomsen <travis.n.thomsen@gmail.com>
2023-07-19 18:34:52 -0500

    move AP into lesson 4

--- Section05/Lesson4/API/src/app.js
+++ Section05/Lesson4/API/src/app.js
@@ -1,16 +1,21 @@
 
 import express from "express";
 import serverless from "serverless-http";
-//const express = require('express');
-//const serverless = require('serverless-http');
+import bodyParser from "body-parser";
+
 const port = process.env.PORT || 3000;
 const app = express();
 
-import importRoutes from './routes/import.routes.js';
-import personRoutes from './routes/person.routes.js';
+import mainRoutes from './routes/main.routes.js'
+import peopleRoutes from './routes/person.routes.js';
+import hobbiesRoutes from './routes/hobby.routes.js'
 
-app.use('/', importRoutes);
-app.use('/', personRoutes);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', peopleRoutes);
+app.use('/', hobbiesRoutes);
+app.use('/', mainRoutes);
 
 if (process.env.PROVIDER === 'aws') {
   exports.handler = serverless(app);
--- Section05/Lesson4/API/src/routes/person.routes.js
+++ Section05/Lesson4/API/src/routes/person.routes.js
@@ -2,15 +2,63 @@ import express from "express";
 
 const router = express.Router();
 const gremlin = require('gremlin');
-const traversal = gremlin.process.AnonymousTraversalSource.traversal;
 const DriverRemoteConnection = gremlin.driver.DriverRemoteConnection;
-
-const g = traversal().withRemote(new DriverRemoteConnection('ws://' + process.env.CLUSTER_ENDPOINT + ':8182/gremlin'));
+const Graph = gremlin.structure.Graph;
+const dcReader = new DriverRemoteConnection('wss://'+ process.env.CLUSTER_ENDPOINT +':8182/gremlin', { mimeType: "application/vnd.gremlin-v2.0+json" });
+const graphReader = new Graph();
+const gR = graphReader.traversal().withRemote(dcReader);
 
 router.get('/people', async function (req, res, next) {
-    const data = await g.V().hasLabel('person').values('id', 'name').toList();
+    const data = await gR.V().hasLabel('person').valueMap(true).toList();
     console.log(data)
-    res.json({ data });
+    
+    res.json(data);
+});
+
+router.get('/people/:id', async function (req, res, next) {
+    const { id } = req.params;
+    const data = await gR.V(id).hasLabel('person').valueMap(true).toList();
+    console.log(data)
+    
+    res.json(data);
+});
+
+router.post('/people/add', async function (req, res, next) {
+    const { name, age, status, born} = req.body;
+
+    const data = await gR.addV('person').property('name', name).property('age', age).property('status', status).property('born', born).next();
+    console.log(data)
+    
+    res.json(data);
+});
+
+router.post('/people/:id/follow', async function (req, res, next) {
+    const { id } = req.params;
+    const { to } = req.body;
+    const date = new Date();
+
+    console.log("id", id);
+    console.log("to", to);
+    console.log("date", date);
+    console.log('query', `g.V('${id}').addE('follows').to(__.V('${to}')).property("since", '${date.toString()}').iterate()`);
+
+    //const data = await gR.V(`'${id}'`).as('v1').V(`'${to}'`).as('v2').addE('follows').from('v1').to('v2').property("since", `'${date.toString()}'`).iterate();
+    const data = await gR.V(`'${id}'`).addE('follows').to(__.V(`'${to}'`)).property("since", `'${date.toString()}'`).iterate();
+    //const data = {'bla': 'test'}; 
+    
+    console.log("data", data);
+    
+    res.json(data);
+});
+
+
+router.delete('/people/:id', async function (req, res, next) {
+    const { id } = req.params;
+
+    const data = await gR.V().hasId(id).drop().next();
+    console.log(data)
+    
+    res.json(data);
 });
 
 export default router;
